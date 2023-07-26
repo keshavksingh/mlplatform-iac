db3fa00b00aa5d5eb1a28aad2d246e5e67b139a6
Keshav Singh <41631964+keshavksingh@users.noreply.github.com>
2023-07-26 15:24:22 -0700

    Update ADBDeploymentRollback.ps1

--- ADBDeploymentRollback.ps1
+++ ADBDeploymentRollback.ps1
@@ -3,15 +3,33 @@ $clientSecret = "<>"
 $tenantId = "<>"
 $resourcegroup = "<>"
 $adbwsname = "<>"
+$subscription= "<>"
+$amlworkspace= "<>"
+$amlAttachedAdbName = "<>"
 
 # Log in to Azure using the service principal
 az login --service-principal --username $clientId --password $clientSecret --tenant $tenantId
 # Set the Azure subscription context (if you have multiple subscriptions)
 az account set --subscription "<>"
 
+#Detach the AML Attached Databricks Compute
+$tokenEndpoint = "https://login.microsoftonline.com/$tenantId/oauth2/token"
+$body = @{"grant_type" = "client_credentials";"client_id" = $clientId;"client_secret" = $clientSecret;"resource" = "https://management.azure.com/"}
+# Convert the token request parameters to URL-encoded form data
+$formData = $body.GetEnumerator() | ForEach-Object { "{0}={1}" -f $_.Key, [System.Web.HttpUtility]::UrlEncode($_.Value) }
+$formData = $formData -join "&"
+$headers = @{ "Content-Type" = "application/x-www-form-urlencoded" }
+$response = Invoke-RestMethod -Uri $tokenEndpoint -Method Post -Headers $headers -Body $formData
+$accessToken = $response.access_token
+# Define the compute delete URL
+$deleteUrl = "https://management.azure.com/subscriptions/$subscription/resourceGroups/$resourcegroup/providers/Microsoft.MachineLearningServices/workspaces/$amlworkspace/computes/"+$amlAttachedAdbName+"?api-version=2023-06-01-preview&underlyingResourceAction=Detach"
+# Send the DELETE request
+$headers = @{ "Authorization" = "Bearer $accessToken" }
+$response = Invoke-RestMethod -Uri $deleteUrl -Method Delete -Headers $headers
+
+#Delete The ADB Workspace
 az config set extension.use_dynamic_install=yes_without_prompt
 Write-Output "Provisioning Azure Databricks workspace..."
-#Delete The ADB Workspace
 $deleteDatabricks = az databricks workspace delete --name $adbwsname --resource-group $resourcegroup --yes | ConvertFrom-Json
 
 $keyVaultName = "<>"
